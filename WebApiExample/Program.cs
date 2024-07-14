3312a27b582635197c95d345e79166a3007a7fe2
chibichaan <xokkko0@mail.ru>
2024-07-14 11:01:17 +0300

    first commit

---
+++ WebApiExample/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text.Json;
+using WebApiExample;
+
+var posts = default(List<Post>);
+
+using (var client = new HttpClient())
+{
+    //запрос
+    var result = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+    result.EnsureSuccessStatusCode(); //проверка что код двухсотый
+
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+        
+    posts = JsonSerializer.Deserialize<List<Post>>(await result.Content.ReadAsStringAsync(), options);
+}
+
+foreach (var post in posts.GroupBy(p=>p.UserId).Select(g=>g.Take(1)).Take(10))
+{
+    Console.WriteLine(JsonSerializer.Serialize(post));
+}
